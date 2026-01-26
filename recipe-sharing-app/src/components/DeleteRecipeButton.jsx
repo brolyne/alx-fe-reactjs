f7ea23f9edb1dd2f612afa17ccc6292846ecacf3
brolyne <shamalabrolyne@gmail.com>
2026-01-26 20:57:05 +0300

    Some updates

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,8 +1,11 @@
 //Component: A simple button that, when clicked, deletes a specific recipe.
 import { useRecipeStore } from './recipeStore';
+import {navigate} from 'react-router-dom';
+
 
 const DeleteRecipeButton = ({ recipeId }) => {
   const deleteRecipe = useRecipeStore(state => state.deleteRecipe);
+  const navigate = useNavigate();
 
   const handleDelete = () => {
     deleteRecipe(recipeId);
