6430e6d2203a9e58ba242bd1a8df94b0e650c4f6
brolyne <shamalabrolyne@gmail.com>
2026-02-01 21:26:24 +0300

    more search parameters

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -5,14 +5,17 @@ export default function Search(){
 
     const [input, setinput]= useState('');
     const [numberofrepos, setnumberofrepos]= useState(0);
+    const [location, setlocation] = useState('');
     const [data,setdata]= useState(null);
     const [loading, setloading]= useState(false);
 
     async function submit(e){
         e.preventDefault();
+        setloading(true);
         console.log("Fetching");
         const data = await fetchUserData(input, numberofrepos);
         setdata(data);
+        setloading(false);
         console.log("Data: ",data);
 
     }
@@ -21,8 +24,13 @@ export default function Search(){
         <div>
             <h2>GitHub User Search</h2>
             <form onSubmit={(e)=>{submit(e)}}>
+                <p>Name</p>
                 <input type="text" onChange={(e)=>{setinput(e.target.value)}}  placeholder="Enter the github name"/>
                 <br/>
+                 <p>Location</p>
+                <input type="text" onChange={(e)=>{setlocation(e.target.value)}}  placeholder="Location"/>
+                <br/>
+                <p>Minimum no. of repos</p>
                 <input type="number" onChange={(e)=>setnumberofrepos(e.target.value)}/>
                 <br/><br/>
                 <button type="submit">Search</button>
--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
 
-export async function fetchUserData(name, numberofrepos){
+export async function fetchUserData(name, numberofrepos, location){
     try {
-        const res = await axios.get(`https://api.github.com/users/${name}`);
+        const res = await axios.get(`https://api.github.com/users/${name}`,{params:{
+            q:`${name} location:${location} repos:>=${numberofrepos}`
+        }});
     //const data = await res.json();
     return res.data;
     } catch (error) {
