9baf2f2770deae9d8bb3bbf746086e8b6dbe3e54
brolyne <shamalabrolyne@gmail.com>
2026-02-19 18:30:36 +0300

    react router advanced

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
-import { Outlet, Link } from 'react-router-dom'
+import { Link, Routes, Route } from 'react-router-dom'
+import ProfileDetails from './ProfileDetails'
+import ProfileSettings from './ProfileSettings'
 
 export default function Profile() {
   return (
     <div>
       <h2>Profile</h2>
-      <nav style={{display: 'flex', gap: 8, marginBottom: 8}}>
+      <nav style={{ display: 'flex', gap: 8, marginBottom: 8 }}>
         <Link to="details">Details</Link>
         <Link to="settings">Settings</Link>
       </nav>
 
-      <Outlet />
+      <Routes>
+        <Route index element={<ProfileDetails />} />
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
     </div>
   )
 }
